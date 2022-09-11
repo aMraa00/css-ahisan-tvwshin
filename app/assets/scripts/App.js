68978b63f22a0669355ddf1c4eed626984ad2b45
aMraa <abatzorig08@gmail.com>
2022-09-10 18:25:56 -0700

    zaswar

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,1 @@
-import "../styles/styles.css";
-alert("Test");
+alert("Test hiij baina0");
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,10 @@
-const { truncate } = require("fs");
-const path = require("path");
+const path = require("path")
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
-        filename : "bundled.js",
+        filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
     mode: "development",
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader","css-loader"]
-            }
-        ]
-    }
+    watch: true
 };
